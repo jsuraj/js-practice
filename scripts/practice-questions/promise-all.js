64ccfd8e8e5a7f6d17157eb9dde360346b0f3919
Suraj Jagtap <jsurajbaramati@gmail.com>
2024-06-30 15:53:03 +0530

    promise all polyfill

--- scripts/practice-questions/promise-all.js
+++ scripts/practice-questions/promise-all.js
@@ -1,5 +1,4 @@
-// Js interview handbook
-// reference: https://d2a5xnk4s7n8a6.cloudfront.net/spees/w/o/6586a3e9e4b07fb1b52009a2/v/658d338ae4b0590f9ed650e9/u/6592b038e4b026fdf34eeb49/p/assets/pdfs/2023/12/28/658d338ae4b0590f9ed650e9/658d338ae4b0590f9ed650e9_original.pdf?Expires=1719742551&Signature=dkJQmUeunE3CUpP0YxVic0g-C69pnkBgkhQhjKOVWiC0MiVha01QiiDFuzBnm3eWiqgVbbgcbeF2AE4Uf8u0nU0hE1PfhKvzF6jbHTsex4uuB3Us544DexM-ux~7jej8JgW2-D9aI8mSgrg2junw2ShRwJ5Xg374GQH~HB1MpJUZ4W-90TBN9dfOqZPpzo9zbGEVgwra-jCV3oGni5aQKWgB6iyCww49~24EDHgPPZMux9BzZnNFiZxloYP3WV4MT4Pes0k4jz4~~23ksBELCj8FPD6Av9-xjM39njkgNLQ~OJ0zaoLzWv02nvTB1AXu~j1bSaq3i401wRGTK8uY1g__&Key-Pair-Id=APKAW3MHMK54TM3QKYNH#page=40
+// Js interview handbook, page 40: Promise all polyfill
 
 function promiseAll(tasksList) {
   let results = [];
